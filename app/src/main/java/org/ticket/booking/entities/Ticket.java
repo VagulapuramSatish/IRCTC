f740de21ed34710436330c7fbfdd2fa54d6bb77a
satishvagulapuram <satish.vagulapuram@gmail.com>
2024-03-08 18:46:31 -0500

    added getters and setters in Ticket.java

--- app/src/main/java/org/ticket/booking/entities/Ticket.java
+++ app/src/main/java/org/ticket/booking/entities/Ticket.java
@@ -5,6 +5,58 @@ import java.util.Date;
 public class Ticket {
 
     private String userId;
+
+    public Ticket() {
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public Date getDateOfTravel() {
+        return dateOfTravel;
+    }
+
+    public void setDateOfTravel(Date dateOfTravel) {
+        this.dateOfTravel = dateOfTravel;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+
     private String ticketId;
     private String source;
     private String destination;
