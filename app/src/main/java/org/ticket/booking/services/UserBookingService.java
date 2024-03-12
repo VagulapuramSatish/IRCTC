40c783db1e00a98f5d9bfe1e3b3196414b7e0abd
satishvagulapuram <satish.vagulapuram@gmail.com>
2024-03-12 16:54:00 -0400

    added TrainService.java implementation methods

--- app/src/main/java/org/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/org/ticket/booking/services/UserBookingService.java
@@ -8,6 +8,7 @@ import org.ticket.booking.utilities.UserServiceUtil;
 import javax.imageio.IIOException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -48,20 +49,53 @@ public class UserBookingService {
         }
 
 
-        public void fetchBooking(){
-            user.printTickets();
+        public void fetchBookings(){
+            Optional<User> userFetched = userList.stream().filter(user1 -> {
+                return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+            }).findFirst();
+            if(userFetched.isPresent()){
+                userFetched.get().printTickets();
+            }
         }
 
         public Boolean cancelBooking(String ticketId){
-
-            userList.stream().filter(user -> {
-                return user1.getUserId().equals(user.)
-            });
-
+            // todo: Complete this function
             return Boolean.FALSE;
         }
 
+        public List<Train> getTrains(String source, String destination){
+            try{
+                TrainService trainService = new TrainService();
+                return trainService.searchTrains(source, destination);
+            }catch(IOException ex){
+                return new ArrayList<>();
+            }
+        }
 
+        public List<List<Integer>> fetchSeats(Train train){
+            return train.getSeats();
+        }
+
+        public Boolean bookTrainSeat(Train train, int row, int seat) {
+            try{
+                TrainService trainService = new TrainService();
+                List<List<Integer>> seats = train.getSeats();
+                if (row >= 0 && row < seats.size() && seat >= 0 && seat < seats.get(row).size()) {
+                    if (seats.get(row).get(seat) == 0) {
+                        seats.get(row).set(seat, 1);
+                        train.setSeats(seats);
+                        trainService.addTrain(train);
+                        return true; // Booking successful
+                    } else {
+                        return false; // Seat is already booked
+                    }
+                } else {
+                    return false; // Invalid row or seat index
+                }
+            }catch (IOException ex){
+                return Boolean.FALSE;
+            }
+        }
 
 
     }
